84141c152fc1478205a30cbe8679137c436a1c3b
Vladan <vladanvelojic@gmail.com>
2018-02-13 17:17:42 +0100

    flipping animation done; just find nice colors for squares

--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -6,12 +6,14 @@ app.controller('MemoryController', function ($scope, $timeout) {
         [{ value: 5 }, { value: 6 }, { value: 7 }, { value: 8 }],
     ];
 
+    $scope.isFlipped = false;
     $scope.currOpened = null;
     $scope.open = function (obj, index) {
         // ako je otvoreno jedno polje
         if ($scope.currOpened !== null) {
             // otvori i drugo
             obj.opened = true;
+            // onemoguci kliktanje na trece polje dok se prva dva ne zatvore
             disableRestOf();
             // provera da li su prvo otvoreno i drugo otvoreno isti
             if ($scope.currOpened.value === obj.value) {
@@ -19,7 +21,7 @@ app.controller('MemoryController', function ($scope, $timeout) {
                 obj.opened = true;
                 obj.disable = true;
                 $scope.currOpened.disable = true;
-                // enable all others squares
+                // omoguci ponovo sva polja da moze da se klikne
                 enableRestOf();
                 // vrati current na null
                 $scope.currOpened = null;
@@ -31,7 +33,7 @@ app.controller('MemoryController', function ($scope, $timeout) {
                     $scope.currOpened.disable = false;
                     $scope.currOpened = null;
                     obj.opened = false;
-                }, 1000);
+                }, 2000);
             }
             // otvori prvo kliknuto polje; pocetak radnje; stanje
         } else {
